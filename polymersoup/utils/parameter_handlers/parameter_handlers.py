c7401b8759695199fd6e753abc3cad5b8317801b
David Doran <d.doran.1@research.gla.ac.uk>
2020-05-01 14:26:49 +0000

    polymer-specific defaults now added

--- polymersoup/utils/parameter_handlers/parameter_handlers.py
+++ polymersoup/utils/parameter_handlers/parameter_handlers.py
@@ -2,35 +2,12 @@
 This file contains functions for reading parameters from input files and
 creating instance of Parameters class for use in PolymerSoup workflows
 """
-
-from ..type_dicts.parameter_type_dicts import (
-    SILICO_LIBRARY_TYPES,
-    EXTRACTOR_TYPES,
-    POSTPROCESS_TYPES,
-    CORE_PARAM_TYPES
-)
-from ..type_dicts.parameter_instrument_dependencies import (
-    SILICO_LIB_DEPENDENCIES,
-    EXTRACTOR_DEPENDENCIES,
-    POSTPROCESS_DEPENDENCIES
-)
-from .instrument_handlers import check_instrument_info
 from .parameters import Parameters
-
-#  list of parameters which must ALWAYS be supplied in input file
-ESSENTIAL_PARAMS = [
-    "mode",
-    "screening_method",
-    "monomers",
-    "silico",
-    "extractors",
-    "postprocess",
-    "polymer_class"
-]
+from ..type_dicts.parameter_fallbacks import ESSENTIAL_CORE_PARAMS, CORE_CLASSES
 
 def generate_parameters(
     params_dict,
-    param_classes
+    param_classes=CORE_CLASSES
 ):
     """
     Generates instance of Parameters class
@@ -50,29 +27,16 @@ def generate_parameters(
     #  Parameters
     formatted_params = {}
 
-    #  check for any essential parameters that are missing from input file
-    missing_essentials = [
-        param for param in ESSENTIAL_PARAMS
-        if param not in params_dict]
-    if missing_essentials:
-        raise Exception(
-            f'{len(missing_essentials)} essential parameters missing'
-            f'from input parameters file: {missing_essentials}')
-
-    #  get default values for chosen mass spectrometer and / or chromarography
-    instrument_info = check_instrument_info(params_dict)
-
     #  iterate through parameter inner classes and add to formatted_params
     for param_class in param_classes:
         formatted_params[param_class] = generate_param_subobject(
             params_dict=params_dict,
-            param_class=param_class,
-            instrument_info=instrument_info)
+            param_class=param_class)
 
     #  make sure core parameters not specific to nested classes are in
     #  formatted_params to be included as properties in final instance of
     #  Parameters class
-    for param in ESSENTIAL_PARAMS:
+    for param in ESSENTIAL_CORE_PARAMS:
         if param not in formatted_params:
             formatted_params[param] = params_dict[param]
 
@@ -80,86 +44,28 @@ def generate_parameters(
     #  and return
     return Parameters(
         params_dict=formatted_params,
-        type_dict=CORE_PARAM_TYPES,
-        instrument_info=instrument_info,
-        instrument_dependencies=None)
+        params_class="core")
 
-def generate_param_subobject(params_dict, param_class, instrument_info):
+def generate_param_subobject(params_dict, param_class):
     """
     Creates instance of Parameters to be used as an inner class
 
     Args:
         params_dict (dict): dict of parameters and values
         param_class (str): id of inner parameter class
-        instrument_info (dict): default values for parameters defined by
-            instruments (mass spec, chromatography) used in experiment
 
     Returns:
         Parameters: instance of Parameters class
     """
 
-    #  get types of each value, and instrument dependencies for filling in
-    #  non-specified values for parameters
-    type_dict, dependencies = retrieve_param_info(param_class)
-
     #  get parameter class from inputs. NOTE: silico parameters is the only
     #  parameter class with nested inner classes (ms1 and ms2)
     if param_class != "silico":
         return Parameters(
-            params_dict=params_dict[param_class],
-            type_dict=type_dict,
-            instrument_dependencies=dependencies,
-            instrument_info=instrument_info)
-
-    #  add ms1 and ms2 parameters to silico parameters
-    ms1_params = Parameters(
-        params_dict=params_dict["silico"]["ms1"],
-        type_dict=type_dict["ms1"],
-        instrument_dependencies=dependencies,
-        instrument_info=instrument_info)
-    ms2_params = Parameters(
-        params_dict=params_dict["silico"]["ms2"],
-        type_dict=type_dict["ms2"],
-        instrument_dependencies=dependencies,
-        instrument_info=instrument_info)
-
-    #  get final silico params dict ready to create instance of Parameters class
-    #  for silico parameters
-    silico_params = {
-        "ms1": ms1_params,
-        "ms2": ms2_params
-    }
+            params_dict=params_dict,
+            params_class=param_class)
 
     #  return instance of Parameters for silico parameters
     return Parameters(
-        params_dict=silico_params,
-        type_dict={"ms1": Parameters, "ms2": Parameters},
-        instrument_dependencies=dependencies,
-        instrument_info=instrument_info)
-
-def retrieve_param_info(param_class):
-    """
-    Retrieves type_dict and instrument dependencies for a parameter class
-    Args:
-        param_class (str): str id of parameter class
-
-    Raises:
-        Exception: raises exception if parameter is not pre-defined in
-            type_dicts and instrument dependencies
-
-    Returns:
-        dict, dict: type_dict, instrument dependencies
-    """
-
-    #  remove "_parameters" substring if supplied in input file
-    param_class.replace("_parameters", "")
-
-    #  return dependencies from parameter class id
-    if param_class.lower() == "silico":
-        return SILICO_LIBRARY_TYPES, SILICO_LIB_DEPENDENCIES
-    elif param_class.lower() == "extractors":
-        return EXTRACTOR_TYPES, EXTRACTOR_DEPENDENCIES
-    elif param_class.lower() == "postprocess":
-        return POSTPROCESS_TYPES, POSTPROCESS_DEPENDENCIES
-    else:
-        raise Exception(f'{param_class} is invalid')
+        params_dict=params_dict,
+        params_class="silico")
--- polymersoup/utils/parameter_handlers/parameters.py
+++ polymersoup/utils/parameter_handlers/parameters.py
@@ -1,5 +1,19 @@
-from typing import List
-from ..errors import InputTypeError, TypeDictKeyError
+from typing import List, Dict
+from .instrument_handlers import check_instrument_info
+from ..errors import InputTypeError, TypeDictKeyError, MissingParameterValue
+from ..type_dicts.parameter_type_dicts import (
+    CORE_PARAM_TYPES,
+    SILICO_LIBRARY_TYPES,
+    EXTRACTOR_TYPES,
+    POSTPROCESS_TYPES
+)
+from..type_dicts.parameter_fallbacks import (
+    CORE_PARAM_FALLBACKS,
+    SILICO_LIBRARY_FALLBACKS,
+    EXTRACTOR_FALLBACKS,
+    POSTPROCESS_FALLBACKS,
+    ESSENTIAL_CORE_PARAMS
+)
 
 class Parameters():
     """
@@ -13,24 +27,93 @@ class Parameters():
         "type_dict",
         "active_instruments",
         "params_dict",
-        "instrument_dependencies"
+        "fallbacks",
+        "params_class",
+        "sub_params"
     ]
+
+    #  dict of parameter class ids and associated type_dicts for checking
+    #  parameter values
+    PARAM_TYPE_ASSOCIATIONS = {
+        "core": CORE_PARAM_TYPES,
+        "silico": SILICO_LIBRARY_TYPES,
+        "silico_ms2": SILICO_LIBRARY_TYPES["ms2"],
+        "silico_ms1": SILICO_LIBRARY_TYPES["ms1"],
+        "extractors": EXTRACTOR_TYPES,
+        "postprocess": POSTPROCESS_TYPES
+    }
+
+    #  dict of parameter class ids and associated fallback parameters for
+    #  checking parameter values
+    PARAM_FALLBACK_ASSOCIATIONS = {
+        "core": CORE_PARAM_FALLBACKS,
+        "silico": SILICO_LIBRARY_FALLBACKS,
+        "silico_ms1": SILICO_LIBRARY_FALLBACKS["ms1"],
+        "silico_ms2": SILICO_LIBRARY_FALLBACKS["ms2"],
+        "extractors": EXTRACTOR_FALLBACKS,
+        "postprocess": POSTPROCESS_FALLBACKS
+    }
 
     def __init__(
         self,
         params_dict,
-        type_dict,
-        instrument_dependencies,
-        instrument_info
+        params_class
     ):
-
-        self.type_dict = type_dict
+        self.params_class = params_class
         self.params_dict = params_dict
-        self.instrument_dependencies = instrument_dependencies
-        self.active_instruments = instrument_info
+        for param in ESSENTIAL_CORE_PARAMS:
+            if param not in self.params_dict:
+                raise Exception(
+                    f'parameter "{param}" is missing from input file\n'
+                    f'{self.params_class}, {self.params_dict}')
+        self.type_dict, self.fallbacks, self.sub_params = (
+            self.get_param_associations())
+        self.active_instruments = check_instrument_info(params_dict)
 
         #  read params dict and set parameter attributes
         self.set_parameter_attributes()
+
+    def get_param_associations(
+        self,
+        type_associations=PARAM_TYPE_ASSOCIATIONS,
+        fallbacks=PARAM_FALLBACK_ASSOCIATIONS
+    ):
+        """
+        Retrieve type dict and fallbacks for paramerer group
+
+        Args:
+            type_associations (Dict[str, dict], optional): dict of parameter
+                group ids and associated type_dicts.
+                Defaults to PARAM_TYPE_ASSOCIATIONS.
+
+        Raises:
+            Exception: raised if self.params_class not in
+                PARAM_TYPE_ASSOCIATIONS
+
+        Returns:
+            dict, dict: type_dict, fallbacks for parameter group
+        """
+        if self.params_class not in type_associations:
+            raise Exception(
+                f'{self.params_class} not a valid parameter group\n'
+                f'please choose from: {type_associations}')
+
+        if self.params_class in ["silico", "extractors", "postprocess"]:
+            params = self.params_dict[self.params_class]
+        elif self.params_class == "silico_ms1":
+            params = self.params_dict["silico"]["ms1"]
+        elif self.params_class == "silico_ms2":
+            params = self.params_dict["silico"]["ms2"]
+        elif self.params_class == "core":
+            params = self.params_dict
+        else:
+            raise Exception(
+                f'{self.params_class} not a valid parameter group')
+
+        type_dict = type_associations[self.params_class]
+        param_fallbacks = fallbacks[self.params_class]
+
+        return type_dict, param_fallbacks, params
 
     def set_parameter_attributes(self, temp_props=TEMP_PROPS):
         """
@@ -46,35 +129,92 @@ class Parameters():
                 Defaults to TEMP_PROPS.
 
         Raises:
+            MissingParameterValue: raised if the value for an essential
+                parameter is not supplied.
             InputTypeError: raised if the value supplied for a parameter does
-                not match expected type is not convertable to expected type
+                not match expected type is not convertable to expected type.
         """
 
-        #  iterate through parameters and associated values, check which
-        #  parameters have default instrument values and retrieve if necessary
-        #  finally, set each of these parameters as attributes in instance of
+        #  Iterate through parameters and associated values, check which
+        #  parameters have default instrument values and retrieve if necessary.
+        #  Finally, set each of these parameters as attributes in instance of
         #  Parameters class
-        for k, v in self.params_dict.items():
+        for k, v in self.sub_params.items():
             if not v:
-                if k in self.instrument_dependencies["mass_spec"]:
-                    v = self.active_instruments["mass_spec"][k]
-                elif k in self.instrument_dependencies["chromatography"]:
-                    v = self.active_instruments["chromatography"][k]
-            if v:
-                try:
-                    v = self.format_params_attr(k, v)
-                except TypeError:
-                    raise InputTypeError(
+                if k not in self.type_dict["optional"]:
+                    raise MissingParameterValue(
                         key=k,
-                        value=v,
-                        expected_type=self.type_dict[k]
-                    )
+                        type_dict=self.type_dict,
+                        param_class=self.params_class)
+                v = self.check_instrument_default_parameter(k)
+                if not v:
+                    if self.fallbacks:
+                        if k in self.fallbacks:
+                            v = self.fallbacks[k]
+                        else:
+                            raise MissingParameterValue(
+                                key=k,
+                                type_dict=self.type_dict,
+                                instrument_fallback=True,
+                                param_class=self.params_class)
+            try:
+                if v:
+                    if self.params_class != "core":
+                        v = self.format_params_attr(k, v)
+            except TypeError:
+                raise InputTypeError(
+                    key=k,
+                    value=v,
+                    expected_type=self.type_dict[k]
+                )
             setattr(self, k, v)
 
         #  remove attributes which were required to instantiate Parameters but
         #  are no longer needed in later workflows
         for prop in temp_props:
             delattr(self, prop)
+
+    def check_instrument_default_parameter(self, parameter_key):
+        """
+        Checks for instrument default values for a parameter and returns if
+        found. Used to get values for parameters not explicitly stated in input
+        file
+
+        Args:
+            parameter_key (str): parameter id
+
+        Returns:
+            parameter_value: value of parameter from instrument defaults
+        """
+
+        #  if no mass_spec or chromatography instruments have been specified
+        #  in inputs, there will be no instrument defaults so return None
+        if (not self.active_instruments["mass_spec"]
+                and not self.active_instruments["chromatography"]):
+            return None
+
+        #  identify which parameters potentially have set values in active
+        #  instruments
+        instrument_defaults = self.type_dict["instrument_dependent"]
+        inst_only = instrument_defaults["instrument_only"]
+
+        #  check whether parameter is dependent on instruments only and, if so,
+        #  whether value for parameter is given in active instruments
+        for device in inst_only:
+            if parameter_key in self.active_instruments[device]:
+                return self.active_instruments[device][parameter_key]
+
+        #  check whether parameter is dependent on instruments AND polymer type;
+        #  if so, check whether value for parameter is given in active
+        # instruments
+        for device in self.active_instruments:
+            if self.params_dict["polymer_class"] in self.active_instruments[
+                    device]:
+                polymer_specified = self.active_instruments[self.params_dict[
+                    "polymer_class"]]
+                if parameter_key in polymer_specified:
+                    return polymer_specified[parameter_key]
+        return None
 
     def format_params_attr(self, parameter_key, parameter_value):
         """
@@ -97,6 +237,7 @@ class Parameters():
         try:
             expected_type = self.type_dict[parameter_key.lower()]
         except KeyError:
+            raise Exception(self.params_class, parameter_key, self.type_dict)
             raise TypeDictKeyError(
                 key=parameter_key,
                 type_dict=self.type_dict)
@@ -115,9 +256,33 @@ class Parameters():
                 value=parameter_value,
                 expected_type=expected_type)
 
-        subtype = expected_type.__args__[1]
+        #  expand check types to simple dictionaries, in which value types are
+        #  identical
+        check_types.extend([
+            Dict[str, int],
+            Dict[str, float],
+            Dict[str, str],
+            Dict[str, List[int]],
+            Dict[str, List[float]],
+            Dict[str, List[str]]
+        ])
+
+        #  typecast all values in simple dict to appropriate type specified
+        #  in type_dict, and return
+        if expected_type in check_types:
+            return {
+                key: self.format_value(
+                    value=value,
+                    expected_type=expected_type.__args__[1])
+                for key, value in parameter_value.items()
+                if value}
+
+        #  typecast all values in complicated dict to appropriate types
+        #  specified in type_dict, and return
         return {
-            key: self.format_value(value=value, expected_type=subtype)
+            key: self.format_value(
+                value=value,
+                expected_type=self.type_dict[parameter_key])
             for key, value in parameter_value.items()
             if value}
 
